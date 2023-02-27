13c1d92063dc67bf3eb795c78825daa715745681
wahlinski <125895164+wahlinski@users.noreply.github.com>
2023-02-27 08:36:46 +0100

    Added preliminary project structure.
    Added preliminary project structure.

---
+++ labb6/src/labb6/generalSimulator/EventQueue.java
@@ -0,0 +1,23 @@
+package labb6.generalSimulator;
+
+import java.util.ArrayList;
+
+public class EventQueue extends ArrayList<Event>{
+    private int numberOfEvents;
+
+    public EventQueue(){
+       super();
+    }
+    public void addEvent(Event event){
+        
+    }
+    public Event removeEvent(int index){
+
+    }
+    private void moveEvent(int index1, int index2){
+
+    }
+    private void sortEvents(){
+
+    }
+}
---
+++ labb6/src/labb6/generalSimulator/State.java
@@ -0,0 +1,26 @@
+package labb6.generalSimulator;
+
+import java.util.Observable;
+
+@SuppressWarnings("deprecation")
+public abstract class State extends Observable{
+    private int currentTime;
+    private boolean stopped;
+
+    public State(){
+
+    }
+    public int getTime(){
+        return this.currentTime;
+    }
+    public void setTime(){
+
+    }
+    public Boolean isStopped(){
+        return this.stopped;
+    }
+    public abstract void begin();
+    public abstract void stop();
+
+    public abstract void update();
+}
