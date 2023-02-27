e0f5c09bab3d454366d2a0d6eb17cdba938886c8
Samuel <cool23man23@gmail.com>
2023-02-27 16:21:27 +0100

    fixade klart klasserna som räknar ut tid

--- labb6/src/labb6/supermarketSimulator/ArrivalTime.java
+++ labb6/src/labb6/supermarketSimulator/ArrivalTime.java
@@ -6,7 +6,29 @@ Melander Samuel
  */
 package labb6.supermarketSimulator;
 
+import labb6.util.ExponentialRandomStream;
+
 public class ArrivalTime {
 
+    private final double lambda;
+    private final ExponentialRandomStream rand;
+
+    public ArrivalTime(double lambda, long seed) {
+        this.lambda = lambda;
+        rand = new ExponentialRandomStream(lambda, seed);
+    }
+
+    /**
+     * räknar ut när nästa kund kommer till butiken
+     * @param currentTime den nuvarande tiden i state
+     * @return absoluta tiden som händelsen kommer att hända
+     */
+    public double calculate(int currentTime) {
+        return currentTime + rand.next();
+    }
+
+    public double getLambda() {
+        return lambda;
+    }
 
 }
--- labb6/src/labb6/supermarketSimulator/PickTime.java
+++ labb6/src/labb6/supermarketSimulator/PickTime.java
@@ -6,6 +6,33 @@ Melander Samuel
  */
 package labb6.supermarketSimulator;
 
+import labb6.util.UniformRandomStream;
+
 public class PickTime {
-    
+    private final double pMin;
+    private final double pMax;
+    private final UniformRandomStream rand;
+
+    public PickTime(double pMin, double pMax, long seed) {
+        this.pMin = pMin;
+        this.pMax = pMax;
+        rand = new UniformRandomStream(pMin, pMax, seed);
+    }
+
+    /**
+     * räknar ut när kunden har plockat klart sina varor
+     * @param currentTime den nuvarande tiden i state
+     * @return absoluta tiden som händelsen kommer att hända
+     */
+    public double calculate(int currentTime) {
+        return currentTime + rand.next();
+    }
+
+    public double getPMax() {
+        return pMax;
+    }
+
+    public double getPMin() {
+        return pMin;
+    }
 }
