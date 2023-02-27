d3c6690940b371db3d6b6c5714a69baf18e7fb4d
Samuel <cool23man23@gmail.com>
2023-02-27 16:02:17 +0100

    skapade start och stop event

--- labb6/src/labb6/generalSimulator/StopEvent.java
+++ labb6/src/labb6/generalSimulator/StopEvent.java
@@ -1,14 +1,13 @@
 package labb6.generalSimulator;
 
-public class StartEvent extends Event {
-
-    public StartEvent(EventQueue eventQueue, int time) {
+public class StopEvent extends Event{
+    public StopEvent(EventQueue eventQueue, int time) {
         super(eventQueue, time);
     }
 
     @Override
     public void execute(State state) {
         super.execute(state);
-        state.begin();
+        state.stop();
     }
 }
--- labb6/src/labb6/supermarketSimulator/events/ShopOpenEvent.java
+++ labb6/src/labb6/supermarketSimulator/events/ShopOpenEvent.java
@@ -8,9 +8,11 @@ package labb6.supermarketSimulator.events;
 
 import labb6.generalSimulator.Event;
 import labb6.generalSimulator.EventQueue;
+import labb6.generalSimulator.StartEvent;
 import labb6.generalSimulator.State;
+import labb6.supermarketSimulator.ShopState;
 
-public class ShopOpenEvent extends Event {
+public class ShopOpenEvent extends StartEvent {
     public ShopOpenEvent(EventQueue eventQueue, int time) {
         super(eventQueue, time);
     }
@@ -18,5 +20,7 @@ public class ShopOpenEvent extends Event {
     @Override
     public void execute(State state) {
         super.execute(state);
+        EventQueue queue = getEventQueue();
+//        CustomerArrivesEvent e = new CustomerArrivesEvent(queue, ); fixa klart då vi har tidsklasserna
     }
 }
