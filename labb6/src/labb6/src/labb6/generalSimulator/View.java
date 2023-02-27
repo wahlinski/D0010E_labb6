4aef879b0f49b25242474abd80e200fcbc26e12b
wahlinski <125895164+wahlinski@users.noreply.github.com>
2023-02-27 08:33:05 +0100

    Added more of preliminary structure

---
+++ labb6/src/labb6/src/labb6/generalSimulator/View.java
@@ -0,0 +1,11 @@
+package labb6.generalSimulator;
+
+import java.util.Observable;
+import java.util.Observer;
+@SuppressWarnings("deprecation")
+public class View implements Observer{
+
+    @Override
+    public void update(Observable o, Object arg) {}
+    
+}
