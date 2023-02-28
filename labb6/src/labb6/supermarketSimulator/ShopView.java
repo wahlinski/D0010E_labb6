284015b4d53fb1d5def9d806ec17a8bc487ec6f4
K4FFEM4N <80048836+K4ffeMan@users.noreply.github.com>
2023-02-28 15:29:55 +0100

    ShopView är nu färdig

--- labb6/src/labb6/supermarketSimulator/ShopView.java
+++ labb6/src/labb6/supermarketSimulator/ShopView.java
@@ -1,32 +1,34 @@
 /**
-* @author Wåhlin Filip, Abdi Abdi Mohamed, Härdelin Viggo, Melander Samuel
-*/
+ * @author Wåhlin Filip, Abdi Abdi Mohamed, Härdelin Viggo, Melander Samuel
+ */
 package labb6.supermarketSimulator;
 
 import labb6.generalSimulator.Event;
+import labb6.generalSimulator.StartEvent;
 import labb6.generalSimulator.StopEvent;
 import labb6.generalSimulator.View;
 import labb6.supermarketSimulator.events.CustomerEvent;
+import labb6.supermarketSimulator.events.ShopCloseEvent;
 
 import java.util.Observable;
 
 @SuppressWarnings("deprecation")
-public class ShopView extends View{
+public class ShopView extends View {
     private boolean firstCall = true;
 
-    private void viewPrinter(ShopState state){
-        if (this.firstCall){
+    private void viewPrinter(ShopState state) {
+        if (this.firstCall) {
             this.firstCall = false;
             System.out.println("PARAMETRAR");
             System.out.println("========== \n" +
-                "Antal kassor, N..........: "+ Integer.toString(state.getMaxRegisters()) + "\n" +
-                "Max som ryms, M..........: "+ Integer.toString(state.getMaxPeopleInStore()) + "\n" +
-                "Ankomshastighet, lambda..: "+ Double.toString(state.lambda()) + "\n" +
-                "Plocktider, [P_min..Pmax]: ["+ Double.toString(state.pMin()) + ".." + Double.toString(state.pMax()) + "]" + "\n" +
-                "Betaltider, [K_min..Kmax]: ["+ Double.toString(state.kMin()) + ".." + Double.toString(state.kMax()) + "]" + "\n" +
-                "Frö, f...................:"+ Long.toString(state.seed()) + "\n\n" +
-                "FÖRLOPP\n" +
-                "=======");
+                    "Antal kassor, N..........: " + Integer.toString(state.getMaxRegisters()) + "\n" +
+                    "Max som ryms, M..........: " + Integer.toString(state.getMaxPeopleInStore()) + "\n" +
+                    "Ankomsthastighet, lambda..: " + Double.toString(state.lambda()) + "\n" +
+                    "Plocktider, [P_min..Pmax]: [" + Double.toString(state.pMin()) + ".." + Double.toString(state.pMax()) + "]" + "\n" +
+                    "Betaltider, [K_min..Kmax]: [" + Double.toString(state.kMin()) + ".." + Double.toString(state.kMax()) + "]" + "\n" +
+                    "Frö, f...................:" + Long.toString(state.seed()) + "\n\n" +
+                    "FÖRLOPP\n" +
+                    "=======");
 
             System.out.println(String.format("%-10.10s  %-15.15s %-10.10s %-10.10s %-10.10s %-10.10s %-10.10s %-10.10s %-10.10s %-10.10s %-10.10s %-10.10s %-10s",
                     "Tid",
@@ -42,41 +44,79 @@ public class ShopView extends View{
                     "köT",
                     "köar",
                     "[Kassakö..]"
-                    ));
+            ));
         }
     }
 
-    public void infoPrinter(Observable o, Object arg){
-        if (arg instanceof StopEvent){
+    public void infoPrinter(Observable o, Object arg) {
+        if (arg instanceof StopEvent) {
             System.out.printf("%-10.10s  %-15.15s",
                     roundTime(((ShopState) o).getTime()),
                     "Stop");
             System.out.println();
 
-            System.out.println("\n1) Av " + Integer.toString(((ShopState) o).getPeoplePaid()+((ShopState) o).getPeopleMissed()) 
-                + " kunder handlade " + ((ShopState) o).getPeoplePaid() + " medan " + ((ShopState) o).getPeopleMissed() + " missades.\n");
-            System.out.println("2) Total tid " + Integer.toString(((ShopState) o).getMaxRegisters()) 
-                + " kassor varit lediga: " + roundTime(((ShopState) o).getTimeRegistersNotUsed()) + " te");
-            System.out.println("Genomsnittlig ledig kassatid: " + roundTime((((ShopState) o).getTimeRegistersNotUsed()/((ShopState) o).getMaxRegisters())) 
-                + " te (dvs " + roundTime(((((ShopState) o).getTimeRegistersNotUsed()/((ShopState) o).getMaxRegisters())/ ((ShopState) o).getCustomerLastPayedTime())*100) 
-                + "% av tiden från öppning tills sista kunden betalat). \n");
-            System.out.println("3) Total tid " + Integer.toString(((ShopState) o).getPeopleHaveQueued()) 
-                + " kunder tvingats köa: " + roundTime(((ShopState) o).getTimeInQueue()) 
-                + " te. Genomsnittlig kötid: " + roundTime(((ShopState) o).getTimeInQueue()/((ShopState) o).getPeopleHaveQueued()) + " te. \n");
+            System.out.println("\n1) Av " + Integer.toString(((ShopState) o).getPeoplePaid() + ((ShopState) o).getPeopleMissed())
+                    + " kunder handlade " + ((ShopState) o).getPeoplePaid() + " medan " + ((ShopState) o).getPeopleMissed() + " missades.\n");
+            System.out.println("2) Total tid " + Integer.toString(((ShopState) o).getMaxRegisters())
+                    + " kassor varit lediga: " + roundTime(((ShopState) o).getTimeRegistersNotUsed()) + " te");
+            System.out.println("Genomsnittlig ledig kassatid: " + roundTime((((ShopState) o).getTimeRegistersNotUsed() / ((ShopState) o).getMaxRegisters()))
+                    + " te (dvs " + roundTime(((((ShopState) o).getTimeRegistersNotUsed() / ((ShopState) o).getMaxRegisters()) / ((ShopState) o).getCustomerLastPayedTime()) * 100)
+                    + "% av tiden från öppning tills sista kunden betalat). \n");
+            System.out.println("3) Total tid " + Integer.toString(((ShopState) o).getPeopleHaveQueued())
+                    + " kunder tvingats köa: " + roundTime(((ShopState) o).getTimeInQueue())
+                    + " te. Genomsnittlig kötid: " + roundTime(((ShopState) o).getTimeInQueue() / ((ShopState) o).getPeopleHaveQueued()) + " te. \n");
 
         }
-        else{
+
+        else if(arg instanceof StartEvent){
+            System.out.printf("%-10.10s  %-15.15s",
+                    roundTime(((ShopState) o).getTime()),
+                    "Start");
+            System.out.println();
+        }
+
+        else if(arg instanceof ShopCloseEvent){
             ShopState state = ((ShopState) o);
             Event event = ((Event) arg);
             String open = "S";
-            if (state.isOpen()){open = "Ö";}
+            if (state.isOpen()) {
+                open = "Ö";
+            }
 
             String id = "";
             if (event instanceof CustomerEvent c) {
                 id = c.getCustomer().getCustomerID() + "";
             }
 
+            System.out.printf("%-10.10s  %-14.16s %-11.11s %-10.10s %-10.10s %-10.10s %-10.10s %-10.10s %-10.10s %-10.10s %-10.10s %-10.10s %-10s",
+                    roundTime(event.getTime()),
+                    event,
+                    "---",
+                    open,
+                    Integer.toString(state.openRegisters()),
+                    roundTime(state.getTimeRegistersNotUsed()),
+                    Integer.toString(state.getPeopleInStore()),
+                    Integer.toString(state.getPeoplePaid()),
+                    Integer.toString(state.getPeopleMissed()),
+                    Integer.toString(state.getPeopleHaveQueued()),
+                    roundTime(state.getTimeInQueue()),
+                    Integer.toString(state.getCustomerQueue().size()),
+                    state.getCustomerQueue().toString());
+            System.out.println();
+        }
 
+        else {
+            ShopState state = ((ShopState) o);
+            Event event = ((Event) arg);
+            String open = "S";
+            if (state.isOpen()) {
+                open = "Ö";
+            }
+
+            String id = "";
+            if (event instanceof CustomerEvent c) {
+                id = c.getCustomer().getCustomerID() + "";
+            }
 
             System.out.printf("%-10.10s  %-15.15s %-10.10s %-10.10s %-10.10s %-10.10s %-10.10s %-10.10s %-10.10s %-10.10s %-10.10s %-10.10s %-10s",
                     roundTime(event.getTime()),
@@ -101,9 +141,7 @@ public class ShopView extends View{
 
         viewPrinter((ShopState) o); //Denna körs bara 1 gång
 
-        //normalPrinter(o, arg); // Denna ska köras värje gång state ändras!
-
-        infoPrinter(o, arg); // Denna körs en gång efter att när StopEvent triggas.
+        infoPrinter(o, arg); // Denna körs varje gång ett event triggar. Sista körning blir vid stopEvent.
 
     }
 
